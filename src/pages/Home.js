6cb09c8c2630e15a94d17cb8f9888f7d0c6c960b
Michael Gutkind <michaelgutkind@Michaels-MacBook-Pro.local>
2022-09-12 21:07:44 -0400

    added cool home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
+import Carousel from '../components/Carousel';
+import waveGif from '../assets/waves.gif';
+
+
 export const Home = () => {
   return (
     <div className="home" id="home">
-      <h2>Home</h2>
+      {
+     /*{ <Carousel />}*/
+     }
+      <img src={waveGif} alt="my-gif" />
     </div>
   );
 }
 
 export default Home;
+
+//color scheme: https://coolors.co/cad2c5-84a98c-52796f-354f52-2f3e46
